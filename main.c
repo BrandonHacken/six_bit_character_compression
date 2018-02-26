5aaee3b5a63cd384710b0070e3122d1fd9379e74
Brandon Hacken <Brandon@Hacken.me>
2018-02-26 04:40:37 -0500

    Update main.c
    added terminating characters to strings and new decoded string

--- main.c
+++ main.c
@@ -8,12 +8,14 @@ int encode_string(char *char_String, int long long *result, int count_Char, int 
 int decode_string(int long long *result, int count_Char, int char_String_Length);
 
 int main() {
-    char small_String[] = "BAaaZB";  // small example string
-    char large_String[] = "abcdefghijklmnopqrstuv";  // big example string
-    int char_String_Length = strlen(large_String);  // find the length of the string CHANGE PARAMETER DEPEMNDING ON YOUR INPUT
+    //char small_String[] = "BAaaZB\0";  // small example string
+    char small_String[] = "abcdefghijklmnopqrstuv\0";  // big example string
+    
+    int char_String_Length = strlen(small_String);  // find the length of the string CHANGE PARAMETER DEPEMNDING ON YOUR INPUT
+    //printf("string length %i", char_String_Length);
     char char_String[char_String_Length];  // create an array to store up to 10 characters
-    memcpy(char_String, large_String, char_String_Length); // CHANGE MIDDLE PARAMETER DEPEMNDING ON YOUR INPUT
-    printf("%s\n", char_String);
+    memcpy(char_String, small_String, char_String_Length); // CHANGE MIDDLE PARAMETER DEPEMNDING ON YOUR INPUT
+    
     int count_Char = ceil(char_String_Length * 0.1);  // integer that tracks the number of 64 bit integers needed for the string
     int long long result[count_Char];  // pack the 6-bit values into a 64-bit long long int
 
@@ -24,6 +26,7 @@ int main() {
 }
 
 int encode_string(char *char_String, int long long *result, int count_Char, int char_String_Length) {
+    printf("%s\n", char_String);
     int i=0;  // integer that counts from 0 to (64/6 = 10 characters)
     int j=0;  // integer that counts from 0 to count_Char
     int count_Char_Remainder = char_String_Length;  // needed in case there is less than 10 chars stored in a 64 bit integer
@@ -42,6 +45,10 @@ int encode_string(char *char_String, int long long *result, int count_Char, int 
         else if ('A' <= char_String[i] && char_String[i] <= 'Z') {
             char_Result = (char_String[i] - 'A' + 33);  // ex: if char = A - A = 0 next 0 + 1 = 1 signifying the char was the first letter in the alphabet and then add 32 which is making the 6th bit a 1 to signify the char is a upper case letter
             //printf(" test: %i here %i \n", i, char_Result);
+        }
+        else {
+            char_Result = 0;
+            printf(" test: %i here %i \n", i, char_Result);
         }
         
         result[j] = (result[j] << 6) | char_Result;  // shift and then replace the 6 least important bits from result with the 6 least important bits from char_Result
@@ -70,7 +77,11 @@ int decode_string(int long long *result, int count_Char, int char_String_Length)
     int nowtemp = 0;
     long long int temp = 0b111111;
     int decode = 0;
-    char decodenow[char_String_Length];
+    char decodenow[char_String_Length+1];
+    if (count_Char == 1){
+        now = char_String_Length-1;
+    }
+    //printf("what is count_Char %i \n", count_Char);
     while(j <= count_Char) {
         //printf("WHAT IS count_Char_Remainder %i\n",count_Char_Remainder);
         for(i = now; i >= nowtemp; i--) {
@@ -84,6 +95,9 @@ int decode_string(int long long *result, int count_Char, int char_String_Length)
                 // Upper case letter a through z
                 decode = ((decode - 33) + 'A');
             }
+            else {
+                decode = 00;
+            }
             decodenow[i] = (char)decode;
         }
         j++;
@@ -96,9 +110,10 @@ int decode_string(int long long *result, int count_Char, int char_String_Length)
             now = now + 10;
         }
     }
-    /*for ( i =0 ; i < char_String_Length; i++){
-     printf("%c \n", decodenow[i]);
-     }*/
+    decodenow[char_String_Length] = '\0';
+    //for ( i =0 ; i < char_String_Length; i++){
+     //printf("%c \n", decodenow[i]);
+     //}
     printf("word: %s \n", decodenow);
     return (1);
 }
